1a35b96d67c5b83530984a3f7796fd48478ccd3d
rasyfa <rasyfa04@gmail.com>
2022-12-19 17:42:57 +0800

    Atribute dan Method

--- src/tugas03/SepedaMotor.java
+++ src/tugas03/SepedaMotor.java
@@ -1,5 +1,9 @@
 package tugas03;
 
 public class SepedaMotor extends Sepeda{
+    private String jenisMotor;
     
+    void throttle(){
+        System.out.println("NGEEEEENGGGGG.......");
+    }
 }

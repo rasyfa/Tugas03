046abe43a82afe751fb8463e6dfdd21dbc5e7166
rasyfa <rasyfa04@gmail.com>
2022-12-19 17:37:16 +0800

    Menggunakan Method Sett

--- src/tugas03/SepedaAksi.java
+++ src/tugas03/SepedaAksi.java
@@ -2,9 +2,9 @@ package tugas03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk = "Kupu-Kupu";
-        s.warna = "Warna";
-        s.harga = 1000;
+        s.setMerk("Butterfly");
+        s.setWarna("Merah");
+        s.setHarga(1000);
         
         s.cetakInfo();
     }

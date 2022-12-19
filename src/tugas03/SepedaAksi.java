5b28ee35aeaf6149a5bb0b2bb38cf99c362683e5
rasyfa <rasyfa04@gmail.com>
2022-12-19 17:34:24 +0800

    Instance Buat Atribut dan Panggil Method

--- src/tugas03/SepedaAksi.java
+++ src/tugas03/SepedaAksi.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas03;
-
-/**
- *
- * @author LENOVO
- */
 public class SepedaAksi {
-    
+    public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk = "Kupu-Kupu";
+        s.warna = "Warna";
+        s.harga = 1000;
+        
+        s.cetakInfo();
+    }
 }
